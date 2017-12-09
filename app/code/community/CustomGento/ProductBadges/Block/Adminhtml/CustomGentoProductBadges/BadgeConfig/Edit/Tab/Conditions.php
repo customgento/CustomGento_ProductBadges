2acfd4ad2a9df234012320101e31c6870fe5d2e4
Tsvetan Stoychev <ceckoslab@gmail.com>
2017-12-09 15:48:57 +0100

    #4: Fixe a problem when we can not choose a category for condition.

--- app/code/community/CustomGento/ProductBadges/Block/Adminhtml/CustomGentoProductBadges/BadgeConfig/Edit/Tab/Conditions.php
+++ app/code/community/CustomGento/ProductBadges/Block/Adminhtml/CustomGentoProductBadges/BadgeConfig/Edit/Tab/Conditions.php
@@ -55,14 +55,18 @@ class CustomGento_ProductBadges_Block_Adminhtml_CustomGentoProductBadges_BadgeCo
 
         $form->setHtmlIdPrefix('current_badge_');
 
-        /** @todo: May be I should move promo/fieldset.phtml in my module */
-        $renderer = Mage::getBlockSingleton('adminhtml/widget_form_renderer_fieldset')
-            ->setTemplate('promo/fieldset.phtml')
-            ->setNewChildUrl($this->getUrl('*/*/newConditionHtml/form/rule_conditions_fieldset'));
-
         $fieldset = $form->addFieldset('conditions_fieldset', array(
             'legend'=>Mage::helper('customgento_productbadges')->__('Apply the badge only if the following conditions are met (leave blank for all products)')
-        ))->setRenderer($renderer);
+        ));
+
+        Mage::log(get_class($fieldset));
+        Mage::log($fieldset->getHtmlId());
+
+        $renderer = Mage::getBlockSingleton('adminhtml/widget_form_renderer_fieldset')
+            ->setTemplate('promo/fieldset.phtml')
+            ->setNewChildUrl($this->getUrl('*/*/newConditionHtml/form/' . $fieldset->getHtmlId()));
+
+        $fieldset->setRenderer($renderer);
 
         $fieldset->addField('conditions', 'text', array(
             'name' => 'conditions',
