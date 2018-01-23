816cb9e8e8d6f8c27b1e88b15a780185a18657df
Ole Schäfer <ole@customgento.com>
2018-01-23 13:34:25 +0100

    added cronjob to reindex every night

---
+++ app/code/community/CustomGento/ProductBadges/Model/Cron.php
@@ -0,0 +1,8 @@
+<?php
+
+class CustomGento_ProductBadges_Model_Cron{
+
+    public function reindexProductBadges(){
+        Mage::getResourceModel('customgento_productbadges/indexer_productBadges')->rebuild();
+    }
+}
