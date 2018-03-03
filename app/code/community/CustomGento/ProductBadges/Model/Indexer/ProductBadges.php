590cf66f78561f60ae33435156b4e5c4d8100c33
Tsvetan Stoychev <ceckoslab@gmail.com>
2018-03-03 19:37:18 +0100

    #19 - Decoupled Magento core indexer logic from Badges custom indexer class.

--- app/code/community/CustomGento/ProductBadges/Model/Indexer/ProductBadges.php
+++ app/code/community/CustomGento/ProductBadges/Model/Indexer/ProductBadges.php
@@ -1,15 +1,7 @@
 <?php
 
-class CustomGento_ProductBadges_Model_Indexer_ProductBadges extends Mage_Index_Model_Indexer_Abstract
+class CustomGento_ProductBadges_Model_Indexer_ProductBadges
 {
-
-    protected $_matchedEntities
-        = array(
-            Mage_Catalog_Model_Product::ENTITY => array(
-                Mage_Index_Model_Event::TYPE_SAVE,
-                Mage_Index_Model_Event::TYPE_MASS_ACTION
-            ),
-        );
 
     protected $_chunkSize = 500;
 
@@ -39,33 +31,6 @@ class CustomGento_ProductBadges_Model_Indexer_ProductBadges extends Mage_Index_M
         $this->_badgeConfigsCollection = $badgeConfigsCollection
             ->addFiltersNeededForIndexer($storeId);
         $this->_init('customgento_productbadges/indexer_productBadges');
-    }
-
-    protected function _registerEvent(Mage_Index_Model_Event $event)
-    {
-    }
-
-    public function getName()
-    {
-        return 'CustomGento Product Badges';
-    }
-
-    public function getDescription()
-    {
-        return 'Index product badges';
-    }
-
-    protected function _processEvent(Mage_Index_Model_Event $event)
-    {
-        if ($event->getEntity() == Mage_Catalog_Model_Product::ENTITY
-            && $event->getType() == Mage_Index_Model_Event::TYPE_SAVE) {
-            $productId = $event->getDataObject()->getId();
-            $this->getResource()->rebuild(null, array($productId));
-        } else if ($event->getEntity() == Mage_Catalog_Model_Product::ENTITY
-            && $event->getType() == Mage_Index_Model_Event::TYPE_MASS_ACTION) {
-            $productIds = $event->getDataObject()->getProductIds();
-            $this->getResource()->rebuild(null, $productIds);
-        }
     }
 
     public function fetchBadges($productIds = array())
