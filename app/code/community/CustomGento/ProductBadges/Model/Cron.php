97f44a46ce245e23431fcd9cef5cce1bea937751
Tsvetan Stoychev <ceckoslab@gmail.com>
2018-03-18 23:22:42 +0100

    #19: Added cache cleaning to nightly badge rebuild cron.

--- app/code/community/CustomGento/ProductBadges/Model/Cron.php
+++ app/code/community/CustomGento/ProductBadges/Model/Cron.php
@@ -3,8 +3,32 @@
 class CustomGento_ProductBadges_Model_Cron
 {
 
-    public function reindexProductBadges()
+    /**
+     * @param Mage_Cron_Model_Schedule $schedule
+     * @return CustomGento_ProductBadges_Model_Cron
+     */
+    public function reindexProductBadges(Mage_Cron_Model_Schedule $schedule)
     {
-        Mage::getResourceModel('customgento_productbadges/indexer_productBadges')->rebuild();
+        $this->_getProductBadgesIndexerResource()->rebuild();
+        $this->_getCacheHelper()->clearAllBadgeCache();
+
+        return $this;
     }
+
+    /**
+     * @return CustomGento_ProductBadges_Model_Resource_Indexer_ProductBadges
+     */
+    protected function _getProductBadgesIndexerResource()
+    {
+        return Mage::getResourceModel('customgento_productbadges/indexer_productBadges');
+    }
+
+    /**
+     * @return CustomGento_ProductBadges_Helper_Cache
+     */
+    protected function _getCacheHelper()
+    {
+        return Mage::helper('customgento_productbadges/cache');
+    }
+
 }
