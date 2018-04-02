8ae0644f881cc692b9badc78f98f8c018d3bf346
Tsvetan Stoychev <ceckoslab@gmail.com>
2018-04-02 22:47:18 +0200

    #19 - refactored New calls to Mage::getModel()

--- app/code/community/CustomGento/ProductBadges/Model/Queue/Observer.php
+++ app/code/community/CustomGento/ProductBadges/Model/Queue/Observer.php
@@ -7,10 +7,12 @@ class CustomGento_ProductBadges_Model_Queue_Observer
      */
     public function registerBadgeChange(Varien_Event_Observer $observer)
     {
-        $queueRegisterJob = new CustomGento_ProductBadges_Model_Queue_RegisterJob();
+        /** @var CustomGento_ProductBadges_Model_Queue_RegisterJob $queueRegisterJob */
+        $queueRegisterJob = Mage::getModel('customgento_productbadges/queue_registerJob');
         $badgeConfig = $observer->getBadgeConfig();
 
-        $job = new CustomGento_ProductBadges_Model_Queue_Job_BadgeUpdate();
+        /** @var CustomGento_ProductBadges_Model_Queue_Job_BadgeUpdate $job */
+        $job = Mage::getModel('customgento_productbadges/queue_job_badgeUpdate');;
 
         $queueRegisterJob->attemptToRegisterJob($job, $badgeConfig);
     }
@@ -20,12 +22,14 @@ class CustomGento_ProductBadges_Model_Queue_Observer
      */
     public function registerStoreDelete(Varien_Event_Observer $observer)
     {
-        $queueRegisterJob = new CustomGento_ProductBadges_Model_Queue_RegisterJob();
+        /** @var CustomGento_ProductBadges_Model_Queue_RegisterJob $queueRegisterJob */
+        $queueRegisterJob = Mage::getModel('customgento_productbadges/queue_registerJob');
 
         /** @var Mage_Core_Model_Store $store */
         $store = $observer->getEvent()->getStore();
 
-        $job = new CustomGento_ProductBadges_Model_Queue_Job_StoreDelete();
+        /** @var CustomGento_ProductBadges_Model_Queue_Job_StoreDelete $job */
+        $job = Mage::getModel('customgento_productbadges/queue_job_storeDelete');
 
         $queueRegisterJob->attemptToRegisterJob($job, $store);
     }
@@ -35,12 +39,14 @@ class CustomGento_ProductBadges_Model_Queue_Observer
      */
     public function registerStoreGroupDelete(Varien_Event_Observer $observer)
     {
-        $queueRegisterJob = new CustomGento_ProductBadges_Model_Queue_RegisterJob();
+        /** @var CustomGento_ProductBadges_Model_Queue_RegisterJob $queueRegisterJob */
+        $queueRegisterJob = Mage::getModel('customgento_productbadges/queue_registerJob');
 
         /** @var Mage_Core_Model_Store_Group $store */
         $storeGroup = $observer->getEvent()->getStoreGroup();
 
-        $job = new CustomGento_ProductBadges_Model_Queue_Job_StoreGroupDelete();
+        /** @var CustomGento_ProductBadges_Model_Queue_Job_StoreGroupDelete $job */
+        $job = Mage::getModel('customgento_productbadges/queue_job_storeGroupDelete');
 
         $queueRegisterJob->attemptToRegisterJob($job, $storeGroup);
     }
@@ -50,12 +56,14 @@ class CustomGento_ProductBadges_Model_Queue_Observer
      */
     public function registerProductUpdate(Varien_Event_Observer $observer)
     {
-        $queueRegisterJob = new CustomGento_ProductBadges_Model_Queue_RegisterJob();
+        /** @var CustomGento_ProductBadges_Model_Queue_RegisterJob $queueRegisterJob */
+        $queueRegisterJob = Mage::getModel('customgento_productbadges/queue_registerJob');
 
         /** @var Mage_Catalog_Model_Product $store */
         $product = $observer->getEvent()->getProduct();
 
-        $job = new CustomGento_ProductBadges_Model_Queue_Job_ProductUpdate();
+        /** @var CustomGento_ProductBadges_Model_Queue_Job_ProductUpdate $job */
+        $job = Mage::getModel('customgento_productbadges/queue_job_productUpdate');
 
         $queueRegisterJob->attemptToRegisterJob($job, $product);
     }
