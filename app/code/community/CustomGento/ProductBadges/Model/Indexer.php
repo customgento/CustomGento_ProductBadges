72c97bbaaa5c2ee15187505656f5f8e6057558a6
Tsvetan Stoychev <ceckoslab@gmail.com>
2018-05-26 23:35:10 +0200

    #24 - removed not needed code from Indexer class.

--- app/code/community/CustomGento/ProductBadges/Model/Indexer.php
+++ app/code/community/CustomGento/ProductBadges/Model/Indexer.php
@@ -3,44 +3,41 @@ class CustomGento_ProductBadges_Model_Indexer
     extends Mage_Index_Model_Indexer_Abstract
 {
 
-    protected $_matchedEntities
-        = array(
-            Mage_Catalog_Model_Product::ENTITY => array(
-                Mage_Index_Model_Event::TYPE_SAVE,
-                Mage_Index_Model_Event::TYPE_MASS_ACTION
-            ),
-        );
+    protected $_matchedEntities = array();
 
     protected function _construct()
     {
         $this->_init('customgento_productbadges/indexer_productBadges');
     }
 
+    /**
+     * @param Mage_Index_Model_Event $event
+     */
     protected function _registerEvent(Mage_Index_Model_Event $event)
     {
     }
 
+    /**
+     * @return string
+     */
     public function getName()
     {
         return 'CustomGento Product Badges';
     }
 
+    /**
+     * @return string
+     */
     public function getDescription()
     {
         return 'Index product badges';
     }
 
+    /**
+     * @param Mage_Index_Model_Event $event
+    */
     protected function _processEvent(Mage_Index_Model_Event $event)
     {
-        if ($event->getEntity() == Mage_Catalog_Model_Product::ENTITY
-            && $event->getType() == Mage_Index_Model_Event::TYPE_SAVE) {
-            $productId = $event->getDataObject()->getId();
-            $this->getResource()->rebuild(null, array($productId));
-        } else if ($event->getEntity() == Mage_Catalog_Model_Product::ENTITY
-            && $event->getType() == Mage_Index_Model_Event::TYPE_MASS_ACTION) {
-            $productIds = $event->getDataObject()->getProductIds();
-            $this->getResource()->rebuild(null, $productIds);
-        }
     }
 
 }
